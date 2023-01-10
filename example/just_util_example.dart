25895a8295c7472bbfb18127b80f2baf827b4f86
안강민 <hkkm612@gmail.com>
2023-01-10 23:33:52 +0900

    Package 기본 구조 작성

--- example/just_util_example.dart
+++ example/just_util_example.dart
@@ -1,6 +1,6 @@
 import 'package:just_util/just_util.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var awesome = true;
+  print('awesome: $awesome');
 }
--- lib/just_util.dart
+++ lib/just_util.dart
@@ -3,6 +3,14 @@
 /// More dartdocs go here.
 library just_util;
 
-export 'src/just_util_base.dart';
+export 'src/just_bool/just_bool.dart';
+export 'src/just_double/just_double.dart';
+export 'src/just_int/just_int.dart';
+export 'src/just_list/just_list.dart';
+export 'src/just_map/just_map.dart';
+export 'src/just_runes/just_runes.dart';
+export 'src/just_set/just_set.dart';
+export 'src/just_string/just_string.dart';
+export 'src/just_symbol/just_symbol.dart';
 
 // TODO: Export any libraries intended for clients of this package.

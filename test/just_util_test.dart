1a063f09f33b83b5266f7a7a40dcffa2de17715d
안강민 <hkkm612@gmail.com>
2023-01-11 01:09:02 +0900

    Default Test Code 삭제

--- test/just_util_test.dart
+++ test/just_util_test.dart
@@ -1,16 +1,2 @@
-import 'package:just_util/just_util.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = true;
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome, isTrue);
-    });
-  });
-}
+// All tests will be conducted as unit tests.
+// Therefore, this file will be left blank.

563b7625a2fdf600fa789f18ceed2e949d119d03
안강민 <hkkm612@gmail.com>
2023-01-15 23:33:35 +0900

    JSON 예제 코드 추가

--- example/just_util_example.dart
+++ example/just_util_example.dart
@@ -1,6 +1,7 @@
 import 'package:just_util/just_util.dart';
 
 void main() {
-  var awesome = true;
-  print('awesome: $awesome');
+  Map<String, dynamic> json = {};
+  json.setIfPresent(key: 'key1', val: null);
+  json.setIfPresent(key: 'key2', val: 'val2');
 }
